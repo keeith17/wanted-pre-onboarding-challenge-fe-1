bf4aac6da59ef1ef3c5be2315993856120503b75
sara <paranoiaque17@gmail.com>
2022-12-29 16:36:18 +0900

    기본 세팅

---
+++ todo-app/src/MainComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainComponent = () => {
+    return (
+        <div>
+            테스트
+        </div>
+    );
+};
+
+export default MainComponent;
---
+++ todo-app/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MainComponent from './MainComponent';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <MainComponent />
+);
+
